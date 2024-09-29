adb254ad485e74b1cb6941c667e15240cdaaabb0
Sathwik <ps.bits5@gmail.com>
2024-09-29 13:32:09 -0400

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int birthMonth = 0;
+        String trash = "";
+        System.out.print("Enter your birth month (1-12): ");
+        if(in.hasNextInt())
+        {
+            birthMonth = in.nextInt();
+            in.nextLine();
+            if(!(birthMonth > 0 && birthMonth <= 12))
+            {
+                System.out.println("Please enter a birth month from 1-12, not " + birthMonth);
+            }
+            else
+            {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("\nYou said your birth month was " + trash);
+            System.out.println("Run the program again and answer the prompt");
         }
     }
 }
